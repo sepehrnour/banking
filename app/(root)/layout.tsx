f4d2cd368b738733948d59d397fdd8bfad0bdaa6
Sepehr <nourbakhshsepehr@gmail.com>
2024-08-31 17:55:46 -0700

    implement headerbox, animated count, doughnutchart, and basic sidebar

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,11 +1,22 @@
+import Sidebar from '@/components/Sidebar';
+import Image from 'next/image';
 function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: 'Sam', lastName: 'Smith' };
+
   return (
-    <main>
-        SIDEBAR
+    <main className="flex h-screen w-full font-inter">
+        <Sidebar user={loggedIn}/>
+        <div className='flex size-full flex-col'>
+          <div className='root-layout'>
+            <Image src='/icons/logo,svg' width={30} height={30} alt='Menu icon' />
+            <div>
+            </div>
+          </div>
+        </div>
         {children}
     </main>
   );
--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 
 const Home: any = () => {
+  const loggedIn = { firstName : 'Sam' };
+
   return (
-    <div>Home</div>
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox 
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firstName || 'Guest'}
+            subtext="Access and manage your account and transactions effeciently."
+          />
+
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
