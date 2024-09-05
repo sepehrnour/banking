771aaa2717fa064107e979b7d00f52f01f72f9bc
Sepehr Nourbakhsh <sepehr@Sepehrs-MacBook-Pro.local>
2024-09-04 22:12:25 -0700

    implement first draft of virtual bank card

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -29,7 +29,7 @@ const Home: any = () => {
         <RightSidebar
           user={loggedIn}
           transactions={[]}
-          banks={[]}/>
+          banks={[{},{}]}/>
     </section>
   )
 }
--- components/RightSidebar.tsx
+++ components/RightSidebar.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
+import BankCard from './BankCard'
 const RightSidebar = ( { user, transactions, banks } : RightSidebarProps) => {
   return (
     <aside className='right-sidebar'>
@@ -37,6 +38,29 @@ const RightSidebar = ( { user, transactions, banks } : RightSidebarProps) => {
                     </h2>
                 </Link>
             </div>
+
+            {banks?.length > 0 && (
+                <div className='relative flex flex-1 flex-col items-center justify-center gap-5'>
+                    <div className='relative z-10'>
+                        <BankCard 
+                            key={banks[0].$id}
+                            account={banks[0]}
+                            userName={`${user.firstName} ${user.lastName}`}
+                            showBalance={false}>
+                        </BankCard>
+                    </div>
+                    {banks[1] && (
+                        <div className='absolute right-0 top-8 z-0 w-[90%]'>
+                        <BankCard 
+                            key={banks[1].$id}
+                            account={banks[1]}
+                            userName={`${user.firstName} ${user.lastName}`}
+                            showBalance={false}>
+                        </BankCard>
+                        </div>
+                    )}
+                </div>
+            )}
         </section>
     </aside>
   )
